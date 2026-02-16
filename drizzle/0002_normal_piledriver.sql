e8c82a713e42aa3166e23ae93ff1808a05c4c8b0
f-o-x11 <239233855+f-o-x11@users.noreply.github.com>
2026-02-16 00:22:12 -0500

    Fix drizzle config to use MySQL dialect, add MySQL migration

---
+++ drizzle/0002_normal_piledriver.sql
@@ -0,0 +1,8 @@
+DROP TABLE "bots" CASCADE;--> statement-breakpoint
+DROP TABLE "messages" CASCADE;--> statement-breakpoint
+DROP TABLE "process_logs" CASCADE;--> statement-breakpoint
+DROP TABLE "users" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."logType";--> statement-breakpoint
+DROP TYPE "public"."messageRole";--> statement-breakpoint
+DROP TYPE "public"."role";--> statement-breakpoint
+DROP TYPE "public"."status";
